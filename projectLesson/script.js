d227da7e466948b9185b96bc2a0ba0e082c4f6b2
szymo <szymon.basiul@wp.pl>
2020-11-12 22:23:42 +0100

    Polishing plus button features

--- projectLesson/script.js
+++ projectLesson/script.js
@@ -1,41 +1,41 @@
 
 //adding value of button to input field
 const addButtonValueToInputField = (buttonId) => {
-    document.getElementById('calc-screen').value
-    += document.getElementById(buttonId).value;
+    document.getElementById('calc-screen').innerText
+    += document.getElementById(buttonId).innerText;
     
 };
 
 let calculatorMemory = {
+
     savedValue: 0,
+    savedValue2: 0,
     operator: ''
 };
 
-const savingInputValueToMemory = () => {
-    calculatorMemory.savedValue = document.getElementById('calc-screen').value;
-};
-
-const addButtonService = () => {
+const plusButtonService = () => {
+    calculatorMemory.savedValue2 = 0;
+    calculatorMemory.savedValue = document.getElementById('calc-screen').innerText;
     calculatorMemory.operator = '+';
-    savingInputValueToMemory();
-    document.getElementById('calc-screen').innerText = "\n f";
+    document.getElementById('calc-screen').innerText = "";
 };
 
 document.getElementById('calc-button-plus')
 .addEventListener('click', () => {
-    addButtonService();
+    plusButtonService();
     console.log('dziala?');
 });
 
 const showMathematicalResult = () => {
+    calculatorMemory.savedValue2 = document.getElementById('calc-screen').innerText;
     if(calculatorMemory.operator === "+"){
-        document.getElementById('calc-screen').value = 
+        document.getElementById('calc-screen').innerText = 
         (parseInt(calculatorMemory.savedValue) 
-        + parseInt(document.getElementById('calc-screen').value));
+        + parseInt(calculatorMemory.savedValue2));
     }
     
     
-}
+};
 /////////////////////////////////////////////////////////////////////
 
 document.getElementById('calc-button-equal')
@@ -44,13 +44,13 @@ document.getElementById('calc-button-equal')
         console.log(parseInt(calculatorMemory.savedValue)+
             parseInt(document.getElementById('calc-screen').value));*/
             showMathematicalResult()
-    }
+    }  
 });
 
 const addErasingTriggerOnCeButton = () => {
     document.getElementById('calc-button-CE')
     .addEventListener('click', () => {
-        document.getElementById('calc-screen').value = "";
+        document.getElementById('calc-screen').innerText = " ";
     })
 };
 
